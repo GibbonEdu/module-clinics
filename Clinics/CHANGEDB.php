f564924423686f95fc8894771d24eaa58af38cac
Ross Parker <>
2024-11-01 14:02:46 +0800

    v1.10.02

--- Clinics/CHANGEDB.php
+++ Clinics/CHANGEDB.php
@@ -153,3 +153,8 @@ UPDATE gibbonModule SET author='Gibbon Foundation', url='https://gibbonedu.org' 
 ++$count;
 $sql[$count][0] = '1.10.01';
 $sql[$count][1] = "";
+
+//v1.10.02
+++$count;
+$sql[$count][0] = '1.10.02';
+$sql[$count][1] = "";
